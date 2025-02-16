ff710d6e27803c43e528231c055148cdd8a3f1f9
user <user@windowsxp.domain>
2025-02-16 07:35:42 -0700

    packaged module

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
 import { DateRange } from "./lib/date-range";
 import { NumRange } from "./lib/num-range";
+import { Range } from "./lib/range";
 
-module.exports = {
-    Range,
-    NumRange,
-    DateRange
-};
+export { DateRange, NumRange, Range };
